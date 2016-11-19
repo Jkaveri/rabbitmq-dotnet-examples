ef4161903f681252dfd311a095a274b8eda51d16
Henry Nguyen <haiho1992@gmail.com>
2016-11-19 10:40:00 +0700

    Complete example 1: send "Hello message"

--- RabbitMQClient/Example1/src/Receive/Program.cs
+++ RabbitMQClient/Example1/src/Receive/Program.cs
@@ -26,13 +26,16 @@ namespace Receive
                     var message = Encoding.UTF8.GetString(body);
                     Console.WriteLine(" [x] Received {0}", message);
                 };
+
                 channel.BasicConsume(queue: "hello",
                                      noAck: true,
                                      consumer: consumer);
-            }
 
-            Console.WriteLine(" Press [enter] to exit.");
-            Console.ReadLine();
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+
+            }
         }
     }
 }
--- RabbitMQClient/Example1/src/Send/Program.cs
+++ RabbitMQClient/Example1/src/Send/Program.cs
@@ -1,6 +1,7 @@
 ﻿using RabbitMQ.Client;
 using System;
 using System.Text;
+using System.Threading;
 
 namespace Send
 {
@@ -12,23 +13,26 @@ namespace Send
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "Hello",
+                ;
+                channel.QueueDeclare(queue: "hello",
                     durable: false,
                     exclusive: false,
                     autoDelete: false,
                     arguments: null);
                 var message = "Hello World!";
+
                 var body = Encoding.UTF8.GetBytes(message);
-
                 channel.BasicPublish(exchange: "",
-                                     routingKey: "hello",
-                                     basicProperties: null,
-                                     body: body);
-                Console.WriteLine(" [x] Sent {0}", message);
-            }
+                               routingKey: "hello",
+                               basicProperties: null,
+                               body: body);
 
-            Console.WriteLine(" Press [enter] to exit.");
-            Console.ReadLine();
+
+                Console.WriteLine(" [x] Sent {0}", message);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
         }
     }
 }
