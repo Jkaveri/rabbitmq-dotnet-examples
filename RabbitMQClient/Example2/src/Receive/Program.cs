61b88cb451e173f595161b7f7960fbaf909c501c
Henry Nguyen <haiho1992@gmail.com>
2016-11-19 15:10:51 +0700

    Example 2 Work Queues

---
+++ RabbitMQClient/Example2/src/Receive/Program.cs
@@ -0,0 +1,61 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Receive
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "task_queue_2",
+                                        durable: true, // Keep meesage until it was proccessed success full.
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+
+                // In order to defeat that we can use the basicQos method with the prefetchCount = 1
+                // setting.This tells RabbitMQ not to give more than one message to a worker at a
+                // time. Or, in other words, don't dispatch a new message to a worker until it has
+                // processed and acknowledged the previous one. Instead, it will dispatch it to the
+                // next worker that is not still busy.
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+
+                    int dots = message.Split('.').Length - 1;
+                    Thread.Sleep(dots * 1000);
+
+                    Console.WriteLine(" [x] Done");
+
+                    // Let the queue knowns message was delivered success.
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                };
+                
+                // Message acknowledgments are turned on by default. In
+                // previous examples we explicitly turned them off by setting the noAck ("no manual
+                // acks") parameter to true. It's time to remove this flag and send a proper
+                // acknowledgment from the worker, once we're done with a task
+                channel.BasicConsume(queue: "task_queue_2",
+                                     noAck: false,
+                                     consumer: consumer);
+
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
---
+++ RabbitMQClient/Example2/src/Send/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Send
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "task_queue_2",
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var message = GetMessage(args);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                var properties = channel.CreateBasicProperties();
+                // Let RabbitMQ known it should store a message on disk.
+                properties.Persistent = true;
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "task_queue_2",
+                                     basicProperties: properties,
+                                     body: body);
+
+                Console.WriteLine(" [x] Sent {0}", message);
+
+                //Console.WriteLine(" Press [enter] to exit.");
+                //Console.ReadLine();
+            }
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World....");
+        }
+    }
+}
