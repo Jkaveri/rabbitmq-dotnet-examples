702f413ca7106e75567181e1e2a9db0b1a132b45
Henry Nguyen <haiho1992@gmail.com>
2016-11-19 21:12:54 +0700

    Routing example

---
+++ RabbitMQClient/Routing/src/Receive/Program.cs
@@ -0,0 +1,62 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Receive
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                const string exchangeName = "direct_logs";
+                // create queue with random name.
+                var queueName = channel.QueueDeclare().QueueName;
+
+                // declare exchange with type is "fanout"
+                channel.ExchangeDeclare(exchange: exchangeName, type: "direct");
+
+                if (args.Length < 1)
+                {
+                    Console.WriteLine("Usage: {0} [info] [warning] [error]",
+                                            Environment.GetCommandLineArgs()[0]);
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                    return;
+                }
+
+                foreach (var severity in args)
+                {
+                    channel.QueueBind(queue: queueName,
+                                      exchange: "direct_logs",
+                                      routingKey: severity);
+                }
+
+
+                Console.WriteLine(" [*] Waiting for messages.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    var routingKey = ea.RoutingKey;
+                    Console.WriteLine(" [x] Received '{0}':'{1}'",
+                                      routingKey, message);
+                };
+                channel.BasicConsume(queue: queueName,
+                                     noAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
---
+++ RabbitMQClient/Routing/src/Send/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace Send
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                const string exchangeName = "direct_logs";
+
+                var message = GetMessage(args);
+                var type = GetLogType(args);
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.ExchangeDeclare(exchange: exchangeName, type: "direct");
+
+                channel.BasicPublish(exchange: exchangeName,
+                                     routingKey: type,
+                                     basicProperties: null,
+                                     body: body);
+
+                Console.WriteLine(" [x] Sent {0}", message);
+
+                // Console.WriteLine(" Press [enter] to exit.");
+                // Console.ReadLine();
+            }
+        }
+
+        private static string GetLogType(string[] args)
+        {
+            return (args.Length > 0) ? args[0].ToLower() : "info";
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return (args.Length > 1) ? string.Join(" ", args.Skip(1).ToArray()) : "Hello World....";
+        }
+    }
+}
