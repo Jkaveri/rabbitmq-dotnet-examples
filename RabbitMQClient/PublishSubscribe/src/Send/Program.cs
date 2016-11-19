973dbc2ef05ee947a74c951b6e9e6bfad0329fc6
Henry Nguyen <haiho1992@gmail.com>
2016-11-19 15:37:56 +0700

    Publish and subscribe pattern

---
+++ RabbitMQClient/PublishSubscribe/src/Send/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Send
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                const string exchangeName = "logs";
+                var queueName = channel.QueueDeclare().QueueName;
+
+                var message = GetMessage(args);
+                var body = Encoding.UTF8.GetBytes(message);
+
+
+                channel.ExchangeDeclare(exchange: exchangeName, type: "fanout");
+
+                channel.QueueBind(queue: queueName,
+                  exchange: exchangeName,
+                  routingKey: "");
+
+                channel.BasicPublish(exchange: exchangeName,
+                                     routingKey: "",
+                                     basicProperties: null,
+                                     body: body);
+
+                Console.WriteLine(" [x] Sent {0}", message);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World....");
+        }
+    }
+}
